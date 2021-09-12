9786a13f861052d96ee5927ce1ecf36b467836b2
bmehder <bmehder@gmail.com>
2021-09-13 00:53:16 +0200

    adjusted padding on index route

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -4,7 +4,7 @@
 
 <main>
   <Section
-    padding="4rem 2rem"
+    padding="4rem 2rem 6rem"
     background="#02075d"
     color="white"
     title="2 Columns"
@@ -29,7 +29,7 @@
       </p>
     </div>
   </Section>
-  <Section padding="4rem 2rem" title="2/3 + 1/3 Columns">
+  <Section padding="4rem 2rem 6rem" title="2/3 + 1/3 Columns">
     <div class="flex">
       <p style="flex: 2;">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium
@@ -41,7 +41,9 @@
         modi cumque, corporis, fugit autem cum. Quasi pariatur consequuntur aut
         ratione? Minus porro numquam reiciendis delectus ratione quidem et est,
         modi cumque, corporis, fugit autem cum. Quasi pariatur consequuntur aut
-        ratione?
+        ratione? perferendis quia vero dolore illo cum eos eius quos autem eum
+        pariatur. Minus porro numquam reiciendis delectus ratione quidem et est,
+        modi cumque.
       </p>
       <p style="flex: 1;">
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium
@@ -53,7 +55,7 @@
       </p>
     </div>
   </Section>
-  <Section padding="4rem 2rem" background="darkorange" title="3 Columns">
+  <Section padding="4rem 2rem 6rem" background="darkorange" title="3 Columns">
     <div class="flex">
       <p>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laudantium
@@ -82,7 +84,7 @@
     </div>
   </Section>
   <Section
-    padding="4rem 2rem"
+    padding="4rem 2rem 6rem"
     background="#222"
     color="white"
     title="4 Columns"
