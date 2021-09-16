117899bddf03f548095004f22ce3e20184a67396
bmehder <bmehder@gmail.com>
2021-09-16 21:12:23 +0200

    added PageTransition

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,12 +1,25 @@
+<script context="module">
+  export const load = async ({ page }) => ({
+    props: {
+      key: page.path,
+    },
+  })
+</script>
+
 <script>
   import '../app.css'
+  import PageTransition from '$lib/PageTransition.svelte'
   import Head from '$lib/Head.svelte'
   import Header from '$lib/Header.svelte'
   import Footer from '$lib/Footer.svelte'
+
+  export let key
 </script>
 
 <Head />
 
 <Header background="darkorange" />
-<slot />
+<PageTransition refresh={key}>
+  <slot />
+</PageTransition>
 <Footer background="#111" color="white">Acme Company</Footer>
